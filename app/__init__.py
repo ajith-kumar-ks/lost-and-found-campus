1c3489aa0d709ed17f8ea55392ca12cf47f124ff
ajith-kumar-ks <ajithkumarks2007@gmail.com>
2026-09-14 11:40:40 +0530

    added db models

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()   #We're creating our SQLAlchemy database object.db as the connection/interface between Flask and our database.
+
 
 def create_app():
     app = Flask(__name__)
 
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///lost_found.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.init_app(app)
+
     from app.routes import main
     app.register_blueprint(main)
 
+    with app.app_context():
+        from app.models import Item, User
+        db.create_all()
     return app
---
+++ app/models.py
@@ -0,0 +1,29 @@
+from app import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
+    items = db.relationship("Item", backref="reporter", lazy=True)  #item.reporter could give us the user who reported it. user.items gives all items reported by that user.
+    #With lazy=True, SQLAlchemy doesn't fetch the user's items immediately when it fetches the User.It fetches them only when you actually access user.items.
+    def __repr__(self):
+        return f"<User {self.email}>"
+
+    
+
+class Item(db.Model):   #SQLAlchemy turns that conceptually into a database table:
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    category = db.Column(db.String(50), nullable=False)
+    location = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    status = db.Column(db.String(20), default="active")
+    type = db.Column(db.String(10), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)   #user_id connects an item to a particular user.
+
+
+    def __repr__(self):
+        return f"<Item {self.name}>"
