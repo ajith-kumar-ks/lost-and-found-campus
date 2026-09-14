eda81097d39a03c14c1e16ae075442dc27360e3e
ajith-kumar-ks <ajithkumarks2007@gmail.com>
2026-09-14 10:50:19 +0530

    added different routesand pages

--- app/routes.py
+++ app/routes.py
@@ -6,3 +6,23 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def home():
     return render_template('home.html')
+
+@main.route("/lost")
+def lost():
+    return render_template("lost.html")
+
+
+@main.route("/found")
+def found():
+    return render_template("found.html")
+
+
+@main.route("/lost-items")
+def lost_items():
+    return render_template("lost_items.html")
+
+
+@main.route("/found-items")
+def found_items():
+    return render_template("found_items.html")
+
