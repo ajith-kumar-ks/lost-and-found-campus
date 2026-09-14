a0d23774bc62350855e43272c27b7b7bdd8f58e7
ajith-kumar-ks <ajithkumarks2007@gmail.com>
2026-09-14 10:35:04 +0530

    added templates and static folders

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 main = Blueprint('main', __name__)
 
 
 @main.route('/')
 def home():
-    return "Lost and Found Campus"
+    return render_template('home.html')
